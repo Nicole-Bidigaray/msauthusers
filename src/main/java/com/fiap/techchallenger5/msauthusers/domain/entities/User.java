f10fe328f02c4c6a8914cf707f3319f70b57b00c
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-06-10 19:59:03 -0300

    Corrigido mapeamento enum para utilizar String name

--- src/main/java/com/fiap/techchallenger5/msauthusers/domain/entities/User.java
+++ src/main/java/com/fiap/techchallenger5/msauthusers/domain/entities/User.java
@@ -24,6 +24,7 @@ public class User implements UserDetails {
     private String name;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
     public User(String email, String password, UserRole role) {
