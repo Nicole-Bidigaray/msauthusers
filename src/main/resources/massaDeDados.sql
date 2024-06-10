f885100fc5b6c4bfb59e6ebba68d46c99ba29c05
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-06-10 19:58:37 -0300

    Massa inicial user adm corrigido

--- src/main/resources/massaDeDados.sql
+++ src/main/resources/massaDeDados.sql
@@ -1,3 +1,1 @@
--- massaDeDados.sql
-INSERT INTO users (id, name, email, password, role, created_at) VALUES
-('1', 'Admin User', 'admin@example.com', 'encrypted_password', 'ADMIN', CURRENT_TIMESTAMP);
+INSERT INTO users (id,name, email, password, role) VALUES ('80fa5572-1bf8-4f40-bd93-2e5f3eb0cff8','Admin User', 'admin@example.com', 'encrypted_password', 'ADMIN');
