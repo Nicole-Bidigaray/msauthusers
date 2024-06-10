0abcd4c0a2bcd6de157d2437ae809e945dca707d
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-06-10 19:59:57 -0300

    remoção  basePackges

--- src/main/java/com/fiap/techchallenger5/msauthusers/MsauthusersApplication.java
+++ src/main/java/com/fiap/techchallenger5/msauthusers/MsauthusersApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.fiap.techchallenger5.msauthusers.domain.consumer")
+@EnableFeignClients
 public class MsauthusersApplication {
 
 	public static void main(String[] args) {
