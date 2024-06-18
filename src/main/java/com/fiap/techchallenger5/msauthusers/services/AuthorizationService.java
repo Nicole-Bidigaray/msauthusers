148afd458949c44c9fd8c7cb3ede9fa1bae698a8
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-06-17 21:21:43 -0300

    Ativado cache para loadUserByUsername

--- src/main/java/com/fiap/techchallenger5/msauthusers/services/AuthorizationService.java
+++ src/main/java/com/fiap/techchallenger5/msauthusers/services/AuthorizationService.java
@@ -2,6 +2,7 @@ package com.fiap.techchallenger5.msauthusers.services;
 
 import com.fiap.techchallenger5.msauthusers.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +14,7 @@ public class AuthorizationService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
+    @Cacheable("usersDetails")
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email);
