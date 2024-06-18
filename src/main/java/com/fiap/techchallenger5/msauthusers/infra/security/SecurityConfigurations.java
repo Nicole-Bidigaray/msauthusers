ff1009ae75c9540575630aab304239f6ec10f7d5
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-06-17 21:27:29 -0300

    Add mapeamento segurança para outros micro serviços

--- src/main/java/com/fiap/techchallenger5/msauthusers/infra/security/SecurityConfigurations.java
+++ src/main/java/com/fiap/techchallenger5/msauthusers/infra/security/SecurityConfigurations.java
@@ -1,15 +1,14 @@
 package com.fiap.techchallenger5.msauthusers.infra.security;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,23 +28,23 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())    
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                .requestMatchers(HttpMethod.GET, "/h2-console/**").permitAll()
-                .requestMatchers(
-                    "/api-docs/**",
-                    "/swagger-ui/**",
-                    "/swagger-resources/**",
-                    "/configuration/**",
-                    "/webjars/**"                            
-                    ).permitAll()
-                    .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.GET, "/produtos/**").authenticated()
+                    .requestMatchers("/auth/login","/api-docs/**",  "/swagger-resources/**", "/configuration/**", "/webjars/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/auth/register/user").permitAll()
+                    .requestMatchers(HttpMethod.GET,"/swagger-ui/**" ).permitAll()
+                    .requestMatchers(HttpMethod.GET,"/produtos/**" ).authenticated()
+                    .requestMatchers(HttpMethod.POST,"/produtos/**" ).hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.PUT,"/produtos/**" ).hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.POST, "/auth/register/admin").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.DELETE,"/produtos/**" ).hasRole("ADMIN")
+                    .requestMatchers( "/pagamento/**", "/pedidos/**" ).hasRole("ADMIN")
+
+                    .requestMatchers(HttpMethod.POST, "/pagamento/**" ).hasRole("USER")
+                    .requestMatchers( "/carrinho/**" ).hasRole("USER")
                     .anyRequest().authenticated()                        
-                    )
-                    .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                    .httpBasic(Customizer.withDefaults())
-                    .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))          
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                // .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))    
+                // .httpBasic(Customizer.withDefaults())      
                 .build();
     }
 
