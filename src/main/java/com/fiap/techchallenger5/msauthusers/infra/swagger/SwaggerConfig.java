03fb6eb446506de71f72a871808414d169e075e6
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-07-14 22:35:26 -0300

    remoçao server customizado swagger

--- src/main/java/com/fiap/techchallenger5/msauthusers/infra/swagger/SwaggerConfig.java
+++ src/main/java/com/fiap/techchallenger5/msauthusers/infra/swagger/SwaggerConfig.java
@@ -5,24 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
- @Value("${spring.profiles.active:Unknown}")
-    String activeProfile;
-
-    @Value("${spring.application.name:Unknown}")
-    String applicationName;
-
-    @Value("${server.port:Unknown}")
-    String serverPort;
 
     @Bean
     OpenAPI customOpenAPI() {
@@ -34,17 +21,8 @@ public class SwaggerConfig {
                         .version("v1")
                         .description("API do Microserviço de Autenticação e Autorização de Usuários criada exclusivamente para o TechChallenge 5 da FIAP."))
                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
-
-        if("docker".equals(activeProfile)){
-            securityItem.servers(Arrays.asList(getServer()));
-        }
+       
         return securityItem;
     }
 
-     private Server getServer() {
-        Server server = new Server();
-        server.setDescription("Gateway Docker");
-        server.setUrl("http://"+applicationName+":"+serverPort);
-        return server;
-    }
 }
